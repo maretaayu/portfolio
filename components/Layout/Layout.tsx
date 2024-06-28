ff1cc0a1b990732169b7ae02649ffa8b5a7f5b79
Mareta Ayu Handanari <maretahandanari@gmail.com>
2024-06-28 10:41:42 +0700

    feat(layout): add clarity

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -14,6 +14,16 @@ function Layout({ children, pageTitle }: LayoutProps) {
       <Head>
         <title>Mareta | {pageTitle}</title>
         <meta name="description" content="Home page" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "myti20rwvy");`,
+          }}
+        />
       </Head>
       <Header />
       {children}
