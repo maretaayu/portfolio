3a6c2f67c93182d8b8d50e963b3a1854a647acf5
Mareta Ayu Handanari <maretahandanari@gmail.com>
2024-07-01 02:01:05 +0700

    style(card): change radius style

--- components/Card/CardDetail.tsx
+++ components/Card/CardDetail.tsx
@@ -5,7 +5,7 @@ type CardDetailProps = {
 
 function CardDetail({ companyName, details }: CardDetailProps) {
   return (
-    <div className=" bg-subtle py-4 rounded-3xl divide-x  flex divide-border">
+    <div className=" bg-subtle py-4 rounded-lg divide-x  flex divide-border">
       <div className="grid gap-2  px-4">
         <p className="font-light text-xs">Company</p>
         <p className="font-bold text-sm">{companyName}</p>
