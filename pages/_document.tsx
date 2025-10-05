cdcd164348f6b8427d6f333f01ec39d80a01e7fb
Mareta Ayu Handanari <maretahandanari@gmail.com>
2025-10-05 21:02:25 +0700

    feat: Add favicon and web manifest for improved branding and mobile support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="scroll-smooth">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
